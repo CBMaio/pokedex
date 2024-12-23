fba07c6792e17c787f3592ea98a40d66f0dfad9c
Carolina Belen Maio <cmaio@MacBook-Air-de-Carolina.local>
2024-12-23 10:00:09 -0300

    fix: fonts

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
-// eslint-disable-next-line no-undef
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: isProduction ? '/pokedex/' : './',
   plugins: [
     vue(),
     vueDevTools(),
